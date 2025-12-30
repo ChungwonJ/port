0c21d32ce510971ea5c421778b90b2e472983173
Chungwon Jung <wjdcjddnjs1234@gmail.com>
2025-12-30 17:27:17 +0900

    포트 스킬추가

--- data/index.js
+++ data/index.js
@@ -1,13 +1,13 @@
 export const PROJECT = [
   {
     title: "금성아이스쿨,천재밀크T",
-    link: "",
+    link: "https://www.purunet-ischool.co.kr/productGuide/productInfo/productguide02.ks",
     since: "(2024.09 ~ 2024.11)",
     txt: "초등 교육을 위한 문제집 개발입니다. 금성아이스쿨에선 국어 수학 문해력 컨탠츠를 담당하여 개발하였고, 천재에서는 수학컨탠츠를 담당하여 개발하였습니다.",
   },
   {
     title: "아이스크림 에듀",
-    link: "",
+    link: "https://www.home-learn.co.kr/homelearn/intro107/prepare-review-study.html",
     since: "(2024.06 ~ 2024.08)",
     txt: "초등 교육을 위한 문제집 개발입니다. 새롭게 선보이는 개발로서 컨탠츠 개발팀에서 나오는 문제들을 개발하였습니다.",
   },
@@ -16,7 +16,7 @@ export const PROJECT = [
     link: "https://nft.marblex.io/",
     since: "(2023.09 ~ 2024.02)",
     txt: "넷마블 MBX Staking 거래를 할 수있는 NFT 마켓플레이스 입니다. 기존에 존재하였던 마켓플레이스의 추가개발건으로 Swap과 Inventory페이지의 신규개발을 맡아서 페이지와 active 팝업등 개발을 하였습니다.",
-    txtTroble:"기존에 개발되어있던 상태안에서의 추가개발이고 외주를 받아서 하는 개발로서 UI라이브러리 사용이 불가능하였기 때문에, active 효과들을 대응할 수 있도록 컴포넌트를 만들었었고 마켓플레이스를 어플로 만드는게 안되는 상황이라 모바일뷰 개선을 위하여 노력했습니다.",
+    txtTroble: "기존에 개발되어있던 상태안에서의 추가개발이고 외주를 받아서 하는 개발로서 UI라이브러리 사용이 불가능하였기 때문에, active 효과들을 대응할 수 있도록 컴포넌트를 만들었었고 마켓플레이스를 어플로 만드는게 안되는 상황이라 모바일뷰 개선을 위하여 노력했습니다.",
     pcImgSrc: "/images/mbx/mbx0.png",
     pcAlt: "마블렉스 PC버전",
     pcTitle: "Swap 페이지 PC버전",
@@ -29,7 +29,7 @@ export const PROJECT = [
     link: "https://gmm.gold/",
     since: "(2023.07 ~ 2024.07)",
     txt: "GMM 회사를 소개하는 웹사이트 입니다. GMM에서 의뢰가 들어왔던 신규 홈페이지 였고 PC와 반응형 전체를 작업하였습니다",
-    txtTroble:"신규 개발건으로서 GMM 쪽에서 디자인 의뢰가 있었지만 모든 디자인이 png파일로 왔었기 때문에, 전체적인 설계를 중점적으로 생각해서 만들었었고, 컴포넌트 나누는것을 생각하며 노력했습니다.",
+    txtTroble: "신규 개발건으로서 GMM 쪽에서 디자인 의뢰가 있었지만 모든 디자인이 png파일로 왔었기 때문에, 전체적인 설계를 중점적으로 생각해서 만들었었고, 컴포넌트 나누는것을 생각하며 노력했습니다.",
     pcImgSrc: "/images/gmm/gmm0.png",
     pcAlt: "GMM PC버전",
     pcTitle: "GMM 메인페이지 PC버전",
@@ -83,6 +83,31 @@ export const SKILL = [
       {
         skillTxt: "비동기 처리를 이해하고 있습니다.",
       }
+    ],
+  },
+  {
+    title: "Java & SpringBoot",
+    skills: [
+      {
+        skillTxt: "기본적인 Java 문법을 이해하고 있습니다.",
+      },
+      {
+        skillTxt: "MVC 패턴을 이해하고 있습니다.",
+      },
+      {
+        skillTxt: "JPA를 사용하여 API를 개발한 경험이 있습니다.",
+      }
+    ],
+  },
+  {
+    title: "RDBMS & SQL",
+    skills: [
+      {
+        skillTxt: "SQL을 사용하여 데이터를 조회, 삽입, 수정, 삭제할 수 있습니다.",
+      },
+      {
+        skillTxt: "기본적인 데이터베이스 설계 및 정규화에 대해 이해하고 있습니다.",
+      },
     ],
   },
   {
